37c2f0740434b5ebc22e536f7d3673c50dfa4c4a
felip-T <felipegomestaparo@gmail.com>
2025-02-06 00:01:58 -0300

    remove tsserver

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -6,7 +6,7 @@ end)
 
 -- When you don't have mason.nvim installed
 -- You'll need to list the servers installed in your system
-lsp.setup_servers({ 'tsserver', 'eslint', 'mojo'})
+lsp.setup_servers({ 'eslint'})
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
